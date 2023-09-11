3caa5c6c43839ed3f63bafbd64aa951ffa616a29
Task-knr <Task-knr@KNRRC-TASK-18>
2023-09-11 14:25:21 +0530

    sss

---
+++ textProgram/src/textProgram/driverclass.java
@@ -0,0 +1,11 @@
+package textProgram;
+
+public class driverclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello...java");
+
+	}
+
+}
